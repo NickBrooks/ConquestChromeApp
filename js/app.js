93865166da34dedbcf4fffbc8c343026192e3c14
Nick Brooks <nbrooks@thirtyseven.com.au>
2014-06-10 23:20:53 +0930

    Kinda got typeahead working

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var bestPictures = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=vjejqk3x245pnergu7bt6dzs&q=bond&page_limit=10&callback=?'
+});
+ 
+bestPictures.initialize();
+ 
+$('#remote .typeahead').typeahead(null, {
+  name: 'best-pictures',
+  displayKey: 'movies.title',
+  source: bestPictures.ttAdapter()
+});
