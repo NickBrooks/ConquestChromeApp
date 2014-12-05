0300a11d795048ba1cbef97e3b592865defe7ace
Nick Brooks <nbrooks@thirtyseven.com.au>
2014-12-05 16:30:45 +1030

    Functional!

--- ConquestRequestApp/app/AngularFormsApp.js
+++ ConquestRequestApp/app/AngularFormsApp.js
@@ -1,1 +1,7 @@
-﻿var angularFormsApp = angular.module('angularFormsApp', []);
+﻿var app = angular.module('requestApp', []);
+
+//URL Set up
+var apiBaseUrl = 'https://conquestapi.azure-api.net/test/';
+var apiConnection = 'faulkner%20CityOfDarwin/';
+var apiUrl = apiBaseUrl + apiConnection;
+var apiSubscription = '?subscription-key=85305f06728b4dd888b51c7147421d6d';
--- ConquestRequestApp/app/RequestsForm/requestsController.js
+++ ConquestRequestApp/app/RequestsForm/requestsController.js
@@ -1,1 +1,22 @@
-﻿
+﻿app.controller('requestController', function ($scope, $http) {
+    $scope.requests = [];
+    $scope.searched = [];
+
+    $scope.fetchRequests = function (lastName) {
+        fetchRequestsUrl = apiUrl + 'requests/' + lastName + apiSubscription;
+        $scope.lastName = lastName;
+
+        $http.get(fetchRequestsUrl).success(function (requests) {
+            $scope.requests = requests;            
+        }).error(function () {
+            $scope.requests = [];
+            $scope.searched = 1;
+        });
+    };
+
+    $scope.searchRequests = function () {
+        var lastName = document.getElementById("searchBox").value;
+        $scope.fetchRequests(lastName);
+    };
+    
+});
