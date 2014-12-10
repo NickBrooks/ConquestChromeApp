d614253d7ced19b43d4a013a2a37bfe02505f9b6
Nick Brooks <nbrooks@thirtyseven.com.au>
2014-12-10 17:31:04 +1030

    Working Example with Material!!!

--- ConquestRequestApp/app/AngularFormsApp.js
+++ ConquestRequestApp/app/AngularFormsApp.js
@@ -1,7 +1,31 @@
-﻿var app = angular.module('requestApp', []);
+﻿var app = angular.module('requestApp', ['ngRoute', 'appControllers']);
+var appControllers = angular.module('appControllers', []);
+
+//
+//Declare Global Variables
+//
 
 //URL Set up
 var apiBaseUrl = 'https://conquestapi.azure-api.net/test/';
 var apiConnection = 'faulkner%20CityOfDarwin/';
 var apiUrl = apiBaseUrl + apiConnection;
-var apiSubscription = '?subscription-key=85305f06728b4dd888b51c7147421d6d';
+var apiSubscription = '?subscription-key=3f2fe91ca4534c8f88345e98e4e9552b';
+
+//
+//Routing
+//
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: '/app/RequestsForm/requestsList.html',
+            controller: 'requestListController'
+        }).
+        when('/request/:requestID', {
+            templateUrl: '/app/RequestsForm/requestSingle.html',
+            controller: 'requestController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
--- ConquestRequestApp/app/RequestsForm/requestsController.js
+++ ConquestRequestApp/app/RequestsForm/requestsController.js
@@ -1,22 +1,55 @@
-﻿app.controller('requestController', function ($scope, $http) {
-    $scope.requests = [];
-    $scope.searched = [];
+﻿appControllers.controller('requestListController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.requests = [];
+        $scope.searched = [];
 
-    $scope.fetchRequests = function (lastName) {
-        fetchRequestsUrl = apiUrl + 'requests/' + lastName + apiSubscription;
-        $scope.lastName = lastName;
+        //fetch requests
+        $scope.fetchRequests = function (lastName) {
+            Url = apiUrl + 'requests/' + lastName + apiSubscription;
+            $scope.lastName = lastName;
 
-        $http.get(fetchRequestsUrl).success(function (requests) {
-            $scope.requests = requests;            
-        }).error(function () {
-            $scope.requests = [];
-            $scope.searched = 1;
+            $http.get(Url).success(function (requests) {
+                $scope.requests = requests;
+            }).error(function () {
+                $scope.requests = [];
+                $scope.searched = 1;
+            });
+        };
+
+        //run fetch on page load
+        $scope.fetchRequests('Brooks');
+
+    }]);
+
+appControllers.controller('requestController', ['$scope', '$routeParams', '$http', '$window',
+    function ($scope, $routeParams, $http, $window) {
+
+        //get request if param Set
+        Url = apiUrl + 'request/' + $routeParams.requestID + apiSubscription;
+        $http.get(Url).success(function (request) {
+            console.log(request);
+            $scope.request = request;
+
+            //copy request scope to revert reset request form
+            $scope.editableRequest = angular.copy($scope.request);
+        }).error(function (error) {
+            console.log(error);
         });
-    };
 
-    $scope.searchRequests = function () {
-        var lastName = document.getElementById("searchBox").value;
-        $scope.fetchRequests(lastName);
-    };
-    
-});
+        //reset request form
+        $scope.resetRequestForm = function () {
+            $scope.editableRequest = angular.copy($scope.request);
+        };
+
+        //submit request form
+        $scope.submitRequestForm = function () {
+            $http.post(apiUrl + 'request/' + apiSubscription, JSON.stringify($scope.editableRequest)).
+              success(function (data) {
+                  console.log('Success: ' + data);
+                  $scope.request = angular.copy($scope.editableRequest);
+              }).
+              error(function (error) {
+                  console.log('Error: ' + error);
+              });
+        };
+    }]);
